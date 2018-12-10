793a8348d087703edfc43f6008b0bdd34ec32820
SBeard <etacalpha@gmail.com>
2018-12-10 03:21:24 -0500

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,6 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-
-let forceSSL = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  return next();
-};
-
-if (env === 'production') {
-  app.use(forceSSL);
-}
+const path = require('path');
+const express = require('express');
+const app = express();
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/cma-gallery'));
